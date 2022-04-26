28a016061c32cd7dee13581050ba43f6973b59cd
Bilal Khendaf <e1730934@cmaisonneuve.qc.ca>
2022-04-26 11:27:39 -0400

    ProfileView.vue terminé

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,17 +1,184 @@
 <template>
     <div class="section">
-        <div class="has-text-centered">
-            <h1 class="title is-1">
-<!--                TODO: Compléter page profile-->
-                Profile (TODO)
-            </h1>
+        <h1 class="title is-1 has-text-centered">Profile</h1>
+    </div>
+    <div class="container">
+        <div class="section">
+            <div class="content">
+                <div class="message is-danger" v-if="error!==''" style="white-space: pre;
+                border-color: red; border-width: 2px; border-style: solid;">
+                    <p class="message-body">{{ error }}</p>
+                    <div class="message is-success" v-if="success!==''">
+                        <p class="message-body">{{ success }}</p>
+                    </div>
+                </div>
+                <div class="field">
+                    <label for="email" class="label">Email</label>
+                    <div class="control has-icons-left">
+                        <input id="email" type="email" placeholder="e1234567@site.com"
+                               class="input" autocomplete="email" required
+                               v-model="email">
+                        <span class="icon is-small is-left">
+                                <i class="fa fa-envelope"></i></span>
+                    </div>
+                </div>
+                <div class="field">
+                    <label for="username" class="label">Username</label>
+                    <div class="control has-icons-left">
+                        <input id="username" type="text" placeholder="e1234567" class="input"
+                               autocomplete="username" required
+                               v-model="username">
+                        <span class="icon is-small is-left">
+                                <i class="fa fa-envelope"></i></span>
+                    </div>
+                </div>
+                <div class="field">
+                    <label for="password" class="label">Mot de passe</label>
+                    <div class="control has-icons-left">
+                        <input id="password" type="password"
+                               placeholder="*******" class="input"
+                               autocomplete="password"
+                               required
+                               v-model="password">
+                        <span class="icon is-small is-left"><i class="fa fa-lock"></i></span>
+                    </div>
+                </div>
+                <div class="field">
+                    <label for="confirmPassword" class="label">Confirmation mot de passe</label>
+                    <div class="control has-icons-left">
+                        <input id="confirmPassword" type="password"
+                               placeholder="*******" class="input"
+                               autocomplete="password"
+                               required
+                               v-model="confirmPassword">
+                        <span class="icon is-small is-left"><i class="fa fa-lock"></i></span>
+                    </div>
+                </div>
+                <div class="field">
+                    <div class="buttons">
+                        <input id="connexion" class="button is-success"
+                                v-on:click.prevent="update" value="Update">
+                        <input class="button is-danger" value="Annuler"
+                               @click.prevent="$router.push('/')">
+                    </div>
+                    <div class="buttons is-right">
+                        <input class="js-modal-trigger button is-danger"
+                               @click.prevent="showModal = true" value="Supprimer"
+                               >
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal is-active" v-show="showModal" @close="showModal = false">
+        <div class="modal-background"></div>
+        <div class="modal-card">
+            <header class="modal-card-head">
+                <p class="modal-card-title">Confirmation de suppression</p>
+                <button class="delete" aria-label="close"
+                        @click.prevent="showModal = false"></button>
+            </header>
+            <section class="modal-card-body">
+                Voulez-vous vraiment supprimer votre profile?
+            </section>
+            <footer class="modal-card-foot">
+                <input class="button has-text-weight-bold is-danger"
+                       @click.prevent="handlerSupprimer" value="Supprimer">
+                <input class="button" @click.prevent="showModal = false" value="Retour">
+            </footer>
         </div>
     </div>
 </template>
 
 <script>
+import { svrURL } from '@/constants';
+
 export default {
     name: 'ProfileView',
+    data() {
+        return {
+            email: '',
+            username: '',
+            password: '',
+            confirmPassword: '',
+            error: '',
+            success: '',
+            showModal: false,
+        };
+    },
+    methods: {
+        async update() {
+            this.error = '';
+            const {
+                email,
+                username,
+                password,
+                confirmPassword,
+            } = this;
+            const regEmail = /@/;
+            const regUsername = (/^\w/);
+
+            if (!regEmail.test(email)) { this.error += ('Le courriel doit contenir le symbole @.\n'); }
+            if (email.length < 4) { this.error += 'Le courriel doit contenir plus de 4 caractères.\n'; }
+
+            if (!regUsername.test(username)) {
+                this.error += ('Les caractères permis pour le username sont A-z, a-z, 0-9 et le caractère souligné.\n');
+            }
+            if (username.length > 20 || username < 5) { this.error += 'Le username doit contenir au moins 5 caractères et au maximum 20.\n'; }
+            if (password.length < 6) { this.error += 'Le mot de passe doit contenir au moins 6 caractères.\n'; }
+            if ((password !== confirmPassword)) {
+                this.error += ('Les mots de passe ne correspondent pas\n');
+            }
+            if (this.error === '') {
+                const bearerToken = `bearer ${this.$store.state.token}`;
+                await fetch(`${svrURL}/user`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        authorization: bearerToken,
+                    },
+                    body: JSON.stringify({
+                        email,
+                        username,
+                        password,
+                    }),
+                })
+                    .then((response) => {
+                        if (response.status === 200) {
+                            this.success = ('Les informations de l\'utilisateur ont été modifiées');
+                            this.$router.push({ name: 'home' });
+                        } else {
+                            this.error = response.message;
+                        }
+                    })
+                    .catch(() => {
+                        this.error = ('Erreur lors de l\'inscription');
+                    });
+            }
+        },
+        async handlerSupprimer() {
+            const bearerToken = `bearer ${this.$store.state.token}`;
+            await fetch(`${svrURL}/user`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    authorization: bearerToken,
+                },
+            })
+                .then((response) => {
+                    if (response.status === 200) {
+                        this.success = ('Le compte de l\'utilisateur a été supprimé');
+                        this.$store.dispatch('setToken', '');
+                        this.$router.push({ name: 'home' });
+                    } else {
+                        this.error = response.message;
+                    }
+                })
+                .catch(() => {
+                    this.error = ('Le compte de l\'utilisateur n\'a pas été supprimé');
+                });
+        },
+    },
 };
 </script>
 
