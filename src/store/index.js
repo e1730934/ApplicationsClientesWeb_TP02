b06893d39fe82cdc40feb839ece57045a6008f42
Bilal Khendaf <e1730934@cmaisonneuve.qc.ca>
2022-05-09 11:08:03 -0400

    Fix SignUp Login preloaded username

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import { createStore } from 'vuex';
 export default createStore({
     state: {
         token: '',
+        username: '',
         history: [],
     },
     getters: {},
@@ -11,6 +12,9 @@ export default createStore({
             state.token = token;
             sessionStorage.setItem('token', token);
         },
+        setUsername(state, username) {
+            state.username = username;
+        },
         setHistory(state, history) {
             state.history = history;
         },
@@ -18,6 +22,9 @@ export default createStore({
     actions: {
         setToken({ commit }, token) {
             commit('setToken', token);
+        },
+        setUsername({ commit }, username) {
+            commit('setUsername', username);
         },
         setHistory({ commit }, history) {
             commit('setHistory', history);
